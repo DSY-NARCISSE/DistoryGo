cc0430d2f9476e6afee2908795fd720097a18574
DSY-NARCISSE <benjamin.pereira.contact@gmail.com>
2026-01-19 19:20:40 +0100

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,1 +1,35 @@
+let D = { 
+    gold: 0, 
+    perls: 50, 
+    stats: { hp: 0, atk: 0, def: 0 }, 
+    weapon: null, 
+    head: null, 
+    pet: null, 
+    inv: [], 
+    char: null 
+};
 
+let R = { 
+    active: false, 
+    day: 0, 
+    hp: 100, 
+    max: 100, 
+    atk: 10, 
+    def: 0, 
+    skills: [] 
+};
+
+let C = { 
+    active: false, 
+    hp: 0, 
+    max: 0, 
+    atk: 0, 
+    boss: false 
+};
+
+let Conf = { 
+    auto: false, 
+    tmr: null, 
+    act: null, 
+    spd: 1 
+};
